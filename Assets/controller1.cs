66f1a2bcfd0658b5bd1e53e599549f65b5605fff
xye <xye@xye-26.csusm.edu>
2017-09-16 12:25:57 -0700

    CS 485 Introduction to Unity

---
+++ Assets/controller1.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class controller1 : MonoBehaviour {
+
+	public float speed;
+	private Vector3 moveDirection; 
+
+	// Use this for initialization
+	IEnumerator Start () {
+		//StartCoroutine(Counter());
+		//yield return StartCoroutine(Counter());
+		speed = 10.0f;
+		moveDirection = Vector3.zero;
+		yield break;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		CharacterController controller = GetComponent<CharacterController>();
+		moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+		moveDirection = transform.TransformDirection(moveDirection);
+        moveDirection *= speed;
+        controller.Move(moveDirection * Time.deltaTime);
+	}
+
+	IEnumerator Counter() {
+     for(int i=0; i<10; i++) {
+       Debug.Log (i.ToString() + " Seconds have elapsed");
+       yield return new WaitForSeconds(1.0f);
+     }
+   }
+}
+
+
+
---
+++ Assets/controller2.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class controller2 : MonoBehaviour {
+
+	public float speed;
+	private Vector3 moveDirection; 
+
+	// Use this for initialization
+	IEnumerator Start () {
+		yield return StartCoroutine(Counter());
+		speed = 10.0f;
+		moveDirection = Vector3.zero;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//yield return StartCoroutine(Counter());
+		CharacterController controller = GetComponent<CharacterController>();
+		moveDirection = new Vector3(1, 0, 1);
+		moveDirection = transform.TransformDirection(moveDirection);
+        moveDirection *= speed;
+        controller.Move(moveDirection * Time.deltaTime);
+	}
+
+	IEnumerator Counter() {
+     for(int i=0; i<10; i++) {
+       Debug.Log (i.ToString() + " Seconds have elapsed");
+       yield return new WaitForSeconds(1.0f);
+     }
+   }
+}
+
+
+
